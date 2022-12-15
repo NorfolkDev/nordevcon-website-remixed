efd88b2ec95ac2b2444253fb73962d3e2b5a92f4
Tom Sherman <the.tomsherman@gmail.com>
2022-12-15 09:02:24 +0000

    Add missing metadata

--- app/root.tsx
+++ app/root.tsx
@@ -8,11 +8,21 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import globalStyles from "./styles/globals.css";
+import config from "~/config.json";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: config.title,
   viewport: "width=device-width,initial-scale=1",
+  description: config.description,
+  "theme-color": config.color,
+  "og:title": config.title,
+  "twitter:title": config.title,
+  "og:description": config.description,
+  "twitter:description": config.description,
+  "og:type": "website",
+  "twitter:card": "summary_large_image",
+  "og:url": config.url,
 });
 
 export const links: LinksFunction = () => [
