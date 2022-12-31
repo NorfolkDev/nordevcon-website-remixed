d77e93b9beedd65e15520eb70753d0ac39cb8454
Tom Sherman <the.tomsherman@gmail.com>
2022-12-31 16:43:20 +0000

    Fix useLayoutEffect warning
    Stupid warning, I know uLE doesnt run on the server just shut up about it grrrrrrr

--- app/routes/__home/session/$id.tsx
+++ app/routes/__home/session/$id.tsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "@remix-run/react";
-import { useLayoutEffect, useRef } from "react";
+import { useRef } from "react";
+import { useIsomorphicLayoutEffect } from "~/lib/use-isomorphic-layout-effect";
 
 export default function SessionModal() {
   // return <Schedule schedule={deserializeSchedule(schedule)} isSharing />;
   const dialog = useRef<HTMLDialogElement>(null);
   const navigate = useNavigate();
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     dialog.current?.showModal();
   }, []);
 
